28299403ce9e6d3fde37155ab74655af1672c02a
Damian Wilkołek <dwilkolek@protonmail.com>
2022-07-05 19:44:28 +0200

    feat: update workflow and auto update process

--- build.sh
+++ build.sh
@@ -1,3 +1,17 @@
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags="-X 'main.version=$1'" -o=bin/moxy-windows.exe
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags="-X 'main.version=$1'" -o=bin/moxy-mac
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-X 'main.version=$1'" -o=bin/moxy-linux
+#!/bin/bash
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags="-X 'main.version=$1'" -o=bin/moxy-windows-amd64.exe
+if [[ ! -e bin/moxy-windows-amd64.exe ]]; then
+    exit 1
+fi
+
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags="-X 'main.version=$1'" -o=bin/moxy-darwin-amd64
+if [[ ! -e bin/moxy-darwin-amd64 ]]; then
+    exit 1
+fi
+
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-X 'main.version=$1'" -o=bin/moxy-linux-amd64
+if [[ ! -e bin/moxy-linux-amd64 ]]; then
+    exit 1
+fi
+
+ls -la bin
