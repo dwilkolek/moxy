5c857c240a3e863a97707861d26afd07b2d1581e
Damian Wilkołek <dwilkolek@protonmail.com>
2022-07-05 19:52:25 +0200

    add fallback

--- build.sh
+++ build.sh
@@ -3,15 +3,18 @@ CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags="-X 'main.version=$1'"
 if [[ ! -e bin/moxy-windows-amd64.exe ]]; then
     exit 1
 fi
+cp bin/moxy-windows bin/moxy-windows.exe
 
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags="-X 'main.version=$1'" -o=bin/moxy-darwin-amd64
 if [[ ! -e bin/moxy-darwin-amd64 ]]; then
     exit 1
 fi
+cp bin/moxy-darwin-amd64 bin/moxy-mac
 
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-X 'main.version=$1'" -o=bin/moxy-linux-amd64
 if [[ ! -e bin/moxy-linux-amd64 ]]; then
     exit 1
 fi
+cp bin/moxy-linux-amd64 bin/moxy-linux
 
 ls -la bin
