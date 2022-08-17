044508ab9aeda95eee20c585ca1e6e6cdff50000
Damian Wilkołek <dwilkolek@protonmail.com>
2022-08-17 18:32:12 +0200

    add json validation

--- config/config.go
+++ config/config.go
@@ -7,24 +7,27 @@ import (
 	"strings"
 
 	"github.com/dwilkolek/moxy/internal/logger"
+	"github.com/go-playground/validator/v10"
 )
 
 type MoxyConfig struct {
-	Tunnel   TunnelConfig           `json:"tunnel"`
-	Services map[string]ProxyConfig `json:"services"`
+	Tunnel   TunnelConfig           `json:"tunnel" validate:"required"`
+	Services map[string]ProxyConfig `json:"services" validate:"required"`
 }
 
 type TunnelConfig struct {
-	UserAndHost      string `json:"userAndHost"`
-	PathToPrivateKey string `json:"pathToPrivateKey"`
-	Destination      string `json:"destination"`
+	UserAndHost      string `json:"userAndHost" validate:"required"`
+	PathToPrivateKey string `json:"pathToPrivateKey" validate:"required"`
+	Destination      string `json:"destination" validate:"required"`
 }
 
 type ProxyConfig struct {
-	Port      int               `json:"port"`
+	Port      int               `json:"port" validate:"required,numeric"`
 	Headers   map[string]string `json:"headers"`
-	AllowCors bool              `json:"allowCors"`
+	AllowCors bool              `json:"allowCors" validate:"required,numeric"`
 }
+
+var Validator = validator.New()
 
 func NewConfig(arg string) (*MoxyConfig, error) {
 	fileLocation := findFileLocation(arg)
@@ -32,8 +35,13 @@ func NewConfig(arg string) (*MoxyConfig, error) {
 	if err != nil {
 		return &MoxyConfig{}, err
 	}
+
 	var objmap MoxyConfig
 	json.Unmarshal([]byte(jsonFile), &objmap)
+	err = Validator.Struct(objmap)
+	if err != nil {
+		return &MoxyConfig{}, err
+	}
 	return &objmap, nil
 }
 
